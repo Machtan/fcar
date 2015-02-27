679773987b2ad88fa716696611ec6594fce19997
Rabka <smot@itu.dk>
2015-02-27 17:38:06 +0100

    TestGame v 0.3

--- fcar/fcar/Program.fs
+++ fcar/fcar/Program.fs
@@ -1,7 +1,7 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open TestPlatformerGame
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let main argv =
+    use g = new Game1()
+    g.Run()
+    0
