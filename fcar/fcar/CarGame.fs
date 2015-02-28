1e676b96ee14fb03a6cad7f54c243bc4a25833d5
Jakob Lautrup Nysom <jaln@itu.dk>
2015-02-28 20:26:01 +0100

    Rotation + new sprite

--- fcar/fcar/CarGame.fs
+++ fcar/fcar/CarGame.fs
@@ -7,13 +7,16 @@ open CarActor
 open CarInput
 open CarPhysics
 
+let VectorToAngle (vector: Vector2) =
+    float32 (System.Math.Atan2 (float vector.X, float -vector.Y))
+
 type Cargame () as x =
     inherit Game()
- 
+
     do x.Content.RootDirectory <- "assets"
     let graphics = new GraphicsDeviceManager(x)
     let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
- 
+
     do
         x.Window.Title <- "fcar Super GAME!!!"
         graphics.PreferredBackBufferHeight <- 640
@@ -23,23 +26,32 @@ type Cargame () as x =
 
     let mutable WorldObjects = lazy (
         [
-            (1,"player.png", Player(1,Vector2(0.f,0.f), Vector2(0.f,0.f)), Vector2(32.f,32.f), Circle(16.f));
+            (1,"car.png", Player(1,Vector2(0.f,0.f), Vector2(0.f,0.f)), Vector2(32.f,32.f), Circle(16.f));
         ] |> List.map CreateActor'
     )
 
     let DrawActor (sb:SpriteBatch) actor =
-        if actor.Texture.IsSome 
-            then do sb.Draw(actor.Texture.Value, actor.Pos, Color.White)
+        if actor.Texture.IsSome then
+            let origin =
+                match actor.Geom with
+                | Circle(r) -> Vector2(r, r)
+                | _ -> actor.Pos
+            let angle =
+                match actor.Type with
+                | Player(_, vel, rot) | Active(vel, rot) -> VectorToAngle vel
+                | _ -> 0.0f
+            do sb.Draw(actor.Texture.Value, actor.Pos, System.Nullable(),
+                Color.White, angle, origin, 1.0f, SpriteEffects.None, 0.0f)
         ()
 
     override x.Initialize() =
-        do spriteBatch <- new SpriteBatch(x.GraphicsDevice)  
+        do spriteBatch <- new SpriteBatch(x.GraphicsDevice)
         do base.Initialize()
         ()
- 
+
     override x.LoadContent() =
         ()
- 
+
     override x.Update (gameTime) =
         let HandleInput' = HandleInput (Keyboard.GetState ())
         let current = WorldObjects.Value
@@ -50,7 +62,7 @@ type Cargame () as x =
             |> Move)
         do WorldObjects.Force () |> ignore
         ()
- 
+
     override x.Draw (gameTime) =
         do x.GraphicsDevice.Clear Color.CornflowerBlue
         let DrawActor' = DrawActor spriteBatch
