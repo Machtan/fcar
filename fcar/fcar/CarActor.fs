a4647d95a7a91a64bee736883547b63ea941c8a4
Rabka <smot@itu.dk>
2015-02-28 16:59:54 +0100

    KeyBinding

--- fcar/fcar/CarActor.fs
+++ fcar/fcar/CarActor.fs
@@ -5,30 +5,33 @@ open Microsoft.Xna.Framework.Graphics
 open Microsoft.Xna.Framework.Content
 
 type ActorType =
-    | Player of (int, Vector2, Vector2)
-    | Active of (Vector2, Vector2)
+    | Player of int * Vector2 * Vector2
+    | Active of Vector2 * Vector2
     | Obstacle
 
 type ColGeom =
     | Circle of float
-    | Rect of (float, float)
-    | Compound of (ColGeom, ColGeom)
+    | Rect of float * float
+    | Compound of ColGeom * ColGeom
 
-type WorldActor =
+type Actor =
     {
-        Id: int;
-        Type: ActorType;
-        Pos: Vector2;
-        Texture: Texture2D option;
-        Geom: ColGeom;
+        Id      : int;
+        Type    : ActorType;
+        Pos     : Vector2;
+        Texture : Texture2D option;
+        Geom    : ColGeom;
     }
 
-let CreateActor (content:ContentManager) (texture, atype, pos, geom) =
+let CreateActor (content:ContentManager) (id, texture, atype, pos, geom) =
     let tex =
         if not (System.String.IsNullOrEmpty texture)
         then Some(content.Load texture)
         else None
     {
-        ActorType = atype; Pos = pos;
-        Size = size; Texture = tex; Geom = geom;
+        Id = id; 
+        Type = atype;
+        Pos = pos;
+        Texture = tex; 
+        Geom = geom;
     }
--- fcar/fcar/CarInput.fs
+++ fcar/fcar/CarInput.fs
@@ -8,28 +8,38 @@ let maxvx = 5.0f // Max x velocity
 let inc = 1.0f // x velocity increment
 let gravity = 4.0f // How fast you fall
 
+type keyBinding = 
+    {
+        up:Keys; 
+        down:Keys; 
+        left:Keys; 
+        right:Keys;
+    }
+    
 let HandleInput (kbs:KeyboardState) actor =
-    let rec HandleKeys keys (vel:Vector2, state) =
+    let rec HandleKeys keys (pn, vel:Vector2) =
+        let newXY (incs:float32) max = if (System.Math.Abs vel.X) + (System.Math.Abs incs) > maxvx then max else vel.X + incs
+        let kb = match pn with
+                    | 1 -> {up = Keys.Up; down = Keys.Down; left = Keys.Left; right = Keys.Right;}
+                    | 2 -> {up = Keys.W; down = Keys.S; left = Keys.A; right = Keys.D;}
+                    | _ -> failwith "No keybinding for this player"
         match keys with
         | [] -> vel
         | x :: xs ->
-            let newXY (incs:float32) max = if (System.Math.Abs vel.X) + (System.Math.Abs incs) > maxvx then max else vel.X + incs
-            match x with
-            | Keys.Up    -> HandleKeys xs (Vector2((newXY -inc -maxvx), vel.Y),  state)
-            | Keys.Down  -> HandleKeys xs (Vector2((newXY inc maxvx), vel.Y),    state)
-            | Keys.Left  -> HandleKeys xs (Vector2(vel.Y, (newXY -inc -maxvx)),  state)
-            | Keys.Right -> HandleKeys xs (Vector2(vel.Y, (newXY inc maxvx)),  state)
-            | _ -> HandleKeys xs (vel, state)
-    match actor.ActorType with
-    | Player(s) -> 
-        let init_vel = 
-            match actor.BodyType with
-            | Dynamic(v)   -> v
-            | _            -> Vector2()
-        let velocity = HandleKeys (kbs.GetPressedKeys() |> Array.toList) (init_vel, s)
+            match (x:Keys) with
+                | x when x = kb.up      -> HandleKeys xs (pn, Vector2((newXY -inc -maxvx), vel.Y))
+                | x when x = kb.down    -> HandleKeys xs (pn, Vector2((newXY inc maxvx), vel.Y))
+                | x when x = kb.left    -> HandleKeys xs (pn, Vector2(vel.Y, (newXY -inc -maxvx)))
+                | x when x = kb.right   -> HandleKeys xs (pn, Vector2(vel.Y, (newXY inc maxvx)))
+                | _ -> HandleKeys xs (pn, vel)
+    match actor.Type with
+    | Player(pn,init_vel,rot) ->
+        let velocity = HandleKeys (kbs.GetPressedKeys() |> Array.toList) (pn, init_vel)
         { 
             actor with 
-                BodyType = Dynamic(velocity); 
-                ActorType = Player(Jumping) 
+                Type = Player(pn,velocity,rot) 
         }
     | _ -> actor
+
+
+
