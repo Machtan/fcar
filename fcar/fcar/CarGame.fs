86fbfab06b2531f52e15fe7692682cc16a0d2061
Rabka <smot@itu.dk>
2015-02-28 15:03:24 +0100

    New game

---
+++ fcar/fcar/CarGame.fs
@@ -0,0 +1,31 @@
+﻿module CarGame
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Input
+
+type Cargame () as x =
+    inherit Game()
+ 
+    do x.Content.RootDirectory <- "assets"
+    let graphics = new GraphicsDeviceManager(x)
+    let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+ 
+    do
+        x.Window.Title <- "Test Game"
+        graphics.PreferredBackBufferHeight <- 640
+        graphics.PreferredBackBufferWidth <- 640
+
+    override x.Initialize() =
+        do spriteBatch <- new SpriteBatch(x.GraphicsDevice)  
+        do base.Initialize()
+        ()
+ 
+    override x.LoadContent() =
+        ()
+ 
+    override x.Update (gameTime) =
+        ()
+ 
+    override x.Draw (gameTime) =
+        ()
